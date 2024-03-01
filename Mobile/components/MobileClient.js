e78b23d2c07e0e32e53b9013dbdc632e356d3427
Hanna Yakubouskaya <anniyak@gmail.com>
2024-03-01 13:38:39 +0300

    Заготовка таблицы работы с клиентами мобильной компании

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,45 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './MobileClient.css';
+
+class MobileClient extends React.PureComponent {
+
+  // static propTypes = {
+  //   id: PropTypes.number.isRequired,
+  //   FIO:PropTypes.shape({
+  //     fam: PropTypes.string.isRequired,
+  //     im: PropTypes.string.isRequired,
+  //     otch: PropTypes.string.isRequired,
+  //   }),
+  //   balance: PropTypes.number.isRequired,
+  // };
+  clientMode={
+    view:1,
+    edit:2
+  }
+  state={
+    mode:this.clientMode.view
+  }
+
+  render() {
+
+    console.log("MobileClient id="+this.props.client.id+" render");
+    
+    return (
+      <tr >
+      <td>{this.props.client.fam}</td>
+      <td>{this.props.client.im}</td>
+      <td>{this.props.client.otch}</td>
+      <td>{this.props.client.balance}</td>
+      <td className={this.props.client.balance>=0?"active":"blocked"}>{this.props.client.balance>=0?"active":"blocked"}</td>
+      <td> <input type="button" value="Редактировать" onClick={this.edit} /></td>
+      <td><input type="button" value="Удалить" onClick={this.delete} /></td>
+    </tr>
+    );
+
+  }
+
+}
+
+export default MobileClient;
---
+++ Mobile/components/MobileCompany.js
@@ -0,0 +1,71 @@
+﻿import React from "react";
+import PropTypes from "prop-types";
+
+import MobileClient from "./MobileClient";
+
+import "./MobileCompany.css";
+
+class MobileCompany extends React.PureComponent {
+  // static propTypes = {
+  //   name: PropTypes.string.isRequired,
+  //   clients: PropTypes.arrayOf(
+  //     PropTypes.shape({
+  //       id: PropTypes.number.isRequired,
+  //       fam: PropTypes.string.isRequired,
+  //       im: PropTypes.string.isRequired,
+  //       otch: PropTypes.string.isRequired,
+  //       balance: PropTypes.number.isRequired,
+  //     })
+  //   ),
+  // };
+
+  state = {
+    clients: this.props.clients,
+  };
+
+
+
+  setBalance = (clientId, newBalance) => {
+    let changed = false;
+    let newClients = [...this.state.clients]; // копия самого массива клиентов
+    newClients.forEach((c, i) => {
+      if (c.id == clientId && c.balance != newBalance) {
+        let newClient = { ...c }; // копия объекта изменившегося клиента
+        newClient.balance = newBalance;
+        newClients[i] = newClient;
+        changed = true;
+      }
+    });
+    if (changed) this.setState({ clients: newClients });
+  };
+
+
+
+  render() {
+    console.log("MobileCompany render");
+
+    const clientsCode = this.state.clients.map((client) => {
+      return <MobileClient client={client} key={client.id} />;
+    });
+
+    return (
+      <table className="MobileCompany">
+        <thead>
+          <tr>
+            <th>Фамилия</th>
+            <th>Имя</th>
+            <th>Отчество</th>
+            <th>Баланс</th>
+            <th>Статус</th>
+            <th>Редактировать</th>
+            <th>Удалить</th>
+          </tr>
+        </thead>
+
+        <tbody>{clientsCode}</tbody>
+      </table>
+    );
+  }
+}
+
+export default MobileCompany;
